2c67fc8f42f98506ab0ba35dab9d9b65a4a209e8
SerKor <otworkser@yandex.ru>
2023-09-21 15:42:20 +0300

    4 commit Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,11 @@
-﻿int numberA = 23;
-int numberB = 5;
+﻿//int numberA = 23;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+int numberA = new Random().Next(0, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(0, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
