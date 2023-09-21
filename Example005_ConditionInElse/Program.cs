c1a596c9a2a8d9fb1424742b01d2c7e65b1dfac5
SerKor <otworkser@yandex.ru>
2023-09-21 16:51:29 +0300

    5 Example

---
+++ Example005_ConditionInElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ксюша")
+{
+    Console.WriteLine("Ура, это же КСЮША!");
+}
+
+
+else
+{
+    Console.Write("Привет");
+    Console.Write(username);
+}  
